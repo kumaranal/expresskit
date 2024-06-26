7a3eed598e05e204e1b3df8252396ea5568736b3
akxotiv <akxotiv@gmail.com>
2024-06-26 03:27:32 +0000

    change

--- src/migrations/20240620044512-create-auth.js
+++ src/migrations/20240620044512-create-auth.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("auth", {
+    await queryInterface.createTable("auths", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -38,6 +38,6 @@ module.exports = {
     });
   },
   async down(queryInterface) {
-    await queryInterface.dropTable("auth");
+    await queryInterface.dropTable("auths");
   },
 };
