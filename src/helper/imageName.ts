ff7dedca3af81482dfc6b67160c0eea76f6743d2
mayank-ankr <mayank.ghildiyal@xotiv.com>
2024-07-03 11:21:06 +0530

    changes in routes

--- src/helper/imageName.ts
+++ src/helper/imageName.ts
@@ -2,20 +2,8 @@ import { createCustomError } from "../utils/customError";
 
 const getImageName = async (filePath) => {
   try {
-    const parts = filePath.split("/");
-
-    // Finding the index of 'image'
-    const indexOfImage = parts.indexOf("image");
-
-    // Extracting the base URL and the 'image' part
-    const baseUrl = parts.slice(0, indexOfImage + 2).join("/");
-    let imagePart = parts.slice(indexOfImage, parts.length).join("/");
-
-    // Remove leading '/'
-    if (imagePart.startsWith("/")) {
-      imagePart = imagePart.substring(1);
-    }
-    return imagePart;
+    const parts = filePath.split("/image/")[1];
+    return parts;
   } catch (error) {
     throw createCustomError(error);
   }
