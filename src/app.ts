5d0e49a64315ac294ea18f35865a83c5a01d8bfb
kumaranal <analbiswas180@gmail.com>
2024-07-21 12:47:43 +0530

    change

--- src/app.ts
+++ src/app.ts
@@ -46,65 +46,65 @@ const app = express();
 const port = process.env.PORT || 3000;
 // app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
 
-const allowedOrigins = [
-  "http://localhost:3002",
-  "http://localhost:3001",
-  "http://localhost:3000",
-];
+// const allowedOrigins = [
+//   "http://localhost:3002",
+//   "http://localhost:3001",
+//   "http://localhost:3000",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Reflect the request's credentials as necessary
-    methods: ["GET", "PUT", "POST", "DELETE"],
-  })
-);
-
-app.options("*", cors());
-
-// const wss = new WebSocket.Server({ port: port });
-
-//payment webhook
-// app.use("/api", paymentRoutes);
-// app.use("/api", chatbotRoutes);
-
-// wss.on("connection", function connection(ws) {
-//   ws.send("Welcome New Client!");
-
-//   ws.on("message", function incoming(message) {
-//     wss.clients.forEach(function each(client) {
-//       if (client !== ws && client.readyState === WebSocket.OPEN) {
-//         client.send(message);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
 //       }
-//     });
-//   });
+//     },
+//     credentials: true, // Reflect the request's credentials as necessary
+//     methods: ["GET", "PUT", "POST", "DELETE"],
+//   })
+// );
+
+// app.options("*", cors());
+
+// // const wss = new WebSocket.Server({ port: port });
+
+// //payment webhook
+// // app.use("/api", paymentRoutes);
+// // app.use("/api", chatbotRoutes);
+
+// // wss.on("connection", function connection(ws) {
+// //   ws.send("Welcome New Client!");
+
+// //   ws.on("message", function incoming(message) {
+// //     wss.clients.forEach(function each(client) {
+// //       if (client !== ws && client.readyState === WebSocket.OPEN) {
+// //         client.send(message);
+// //       }
+// //     });
+// //   });
+// // });
+// //rate limiter
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   message: "Too many requests from this IP, please try again after 15 minutes",
 // });
-//rate limiter
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  message: "Too many requests from this IP, please try again after 15 minutes",
-});
-app.use(limiter);
-app.use(helmet());
-app.use(express.json());
-app.use(cookieParser());
+// app.use(limiter);
+// app.use(helmet());
+// app.use(express.json());
+// app.use(cookieParser());
 
 //app routes
 app.get("/", (req, res) => {
   res.send("My World!");
 });
 
-app.use("/api", demoRoutes);
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
-// app.use("/api", aiTranslatorRoutes);
+// app.use("/api", demoRoutes);
+// app.use("/api", authRoutes);
+// app.use("/api", userRoutes);
+// // app.use("/api", aiTranslatorRoutes);
 
 async function startServer() {
   //error handling
