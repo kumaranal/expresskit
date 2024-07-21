d03ae4914f123dcd72217bb35a511cd0b94ba8a1
kumaranal <analbiswas180@gmail.com>
2024-07-21 12:21:43 +0530

    change

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,7 @@ const server = new ApolloServer({
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
 
 const allowedOrigins = [
