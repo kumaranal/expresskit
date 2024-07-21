1f1b0d52ccea6d77f03280a8a9e8f77019bb7c2b
kumaranal <analbiswas180@gmail.com>
2024-07-21 12:51:20 +0530

    change

--- src/app.ts
+++ src/app.ts
@@ -23,115 +23,121 @@ import cors from "cors";
 import WebSocket from "ws";
 import chatbotRoutes from "./routes/chatbot.route";
 
-// Create an instance of ApolloServer
-const server = new ApolloServer({
-  typeDefs: schema,
-  resolvers,
-  context: ({ req, res }) => {
-    const token = req.cookies.accessToken;
-    let user = null;
-    try {
-      if (!token) {
-        throw createCustomError("Invalid Token");
-      }
-      user = getUserFromToken(token);
-    } catch (error) {
-      logger.error("error occures", { error: error });
-    }
-    return { user, req, res };
-  },
-});
-
 const app = express();
 const port = process.env.PORT || 3000;
-// app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
-
-// const allowedOrigins = [
-//   "http://localhost:3002",
-//   "http://localhost:3001",
-//   "http://localhost:3000",
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true, // Reflect the request's credentials as necessary
-//     methods: ["GET", "PUT", "POST", "DELETE"],
-//   })
-// );
-
-// app.options("*", cors());
-
-// // const wss = new WebSocket.Server({ port: port });
-
-// //payment webhook
-// // app.use("/api", paymentRoutes);
-// // app.use("/api", chatbotRoutes);
-
-// // wss.on("connection", function connection(ws) {
-// //   ws.send("Welcome New Client!");
-
-// //   ws.on("message", function incoming(message) {
-// //     wss.clients.forEach(function each(client) {
-// //       if (client !== ws && client.readyState === WebSocket.OPEN) {
-// //         client.send(message);
-// //       }
-// //     });
-// //   });
-// // });
-// //rate limiter
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-//   message: "Too many requests from this IP, please try again after 15 minutes",
-// });
-// app.use(limiter);
-// app.use(helmet());
-// app.use(express.json());
-// app.use(cookieParser());
-
-//app routes
-app.get("/", (req, res) => {
-  res.send("My World!");
+app.listen(port, () => {
+  logger.info(`Server is running on http://localhost:${port}`);
 });
 
-// app.use("/api", demoRoutes);
-// app.use("/api", authRoutes);
-// app.use("/api", userRoutes);
-// // app.use("/api", aiTranslatorRoutes);
+// // Create an instance of ApolloServer
+// const server = new ApolloServer({
+//   typeDefs: schema,
+//   resolvers,
+//   context: ({ req, res }) => {
+//     const token = req.cookies.accessToken;
+//     let user = null;
+//     try {
+//       if (!token) {
+//         throw createCustomError("Invalid Token");
+//       }
+//       user = getUserFromToken(token);
+//     } catch (error) {
+//       logger.error("error occures", { error: error });
+//     }
+//     return { user, req, res };
+//   },
+// });
 
-async function startServer() {
-  //error handling
-  app.use(errorHandlerfn);
+// const app = express();
+// const port = process.env.PORT || 3000;
+// // app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
 
-  // GraphQL endpoint
-  await server.start();
-  server.applyMiddleware({
-    app,
-    path: "/graphql",
-  });
-  // Catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    const error = new Error("API not found");
-    next(error);
-  });
-  sequelize
-    .sync()
-    .then(() => {
-      app.listen(port, () => {
-        logger.info(
-          `Server is running on http://localhost:${port} & for graphql use http://localhost:${port}/graphql`
-        );
-      });
-    })
-    .catch((err: Error) => {
-      logger.error("Unable to connect to the database:", err);
-    });
-}
-startServer();
+// // const allowedOrigins = [
+// //   "http://localhost:3002",
+// //   "http://localhost:3001",
+// //   "http://localhost:3000",
+// // ];
+
+// // app.use(
+// //   cors({
+// //     origin: function (origin, callback) {
+// //       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+// //         callback(null, true);
+// //       } else {
+// //         callback(new Error("Not allowed by CORS"));
+// //       }
+// //     },
+// //     credentials: true, // Reflect the request's credentials as necessary
+// //     methods: ["GET", "PUT", "POST", "DELETE"],
+// //   })
+// // );
+
+// // app.options("*", cors());
+
+// // // const wss = new WebSocket.Server({ port: port });
+
+// // //payment webhook
+// // // app.use("/api", paymentRoutes);
+// // // app.use("/api", chatbotRoutes);
+
+// // // wss.on("connection", function connection(ws) {
+// // //   ws.send("Welcome New Client!");
+
+// // //   ws.on("message", function incoming(message) {
+// // //     wss.clients.forEach(function each(client) {
+// // //       if (client !== ws && client.readyState === WebSocket.OPEN) {
+// // //         client.send(message);
+// // //       }
+// // //     });
+// // //   });
+// // // });
+// // //rate limiter
+// // const limiter = rateLimit({
+// //   windowMs: 15 * 60 * 1000, // 15 minutes
+// //   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// //   message: "Too many requests from this IP, please try again after 15 minutes",
+// // });
+// // app.use(limiter);
+// // app.use(helmet());
+// // app.use(express.json());
+// // app.use(cookieParser());
+
+// //app routes
+// app.get("/", (req, res) => {
+//   res.send("My World!");
+// });
+
+// // app.use("/api", demoRoutes);
+// // app.use("/api", authRoutes);
+// // app.use("/api", userRoutes);
+// // // app.use("/api", aiTranslatorRoutes);
+
+// async function startServer() {
+//   //error handling
+//   app.use(errorHandlerfn);
+
+//   // GraphQL endpoint
+//   await server.start();
+//   server.applyMiddleware({
+//     app,
+//     path: "/graphql",
+//   });
+//   // Catch 404 and forward to error handler
+//   app.use((req, res, next) => {
+//     const error = new Error("API not found");
+//     next(error);
+//   });
+//   sequelize
+//     .sync()
+//     .then(() => {
+//       app.listen(port, () => {
+//         logger.info(
+//           `Server is running on http://localhost:${port} & for graphql use http://localhost:${port}/graphql`
+//         );
+//       });
+//     })
+//     .catch((err: Error) => {
+//       logger.error("Unable to connect to the database:", err);
+//     });
+// }
+// startServer();
